8baeb57c59198c0d3eaeaef2957573c0ac3af970
kacekogar <92035693+kacekogar@users.noreply.github.com>
2021-10-21 11:19:33 +0200

    Add files via upload

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,41 @@
+﻿// ConsoleApplication11.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+// miciek nie zda =]
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+void wypisz(std::vector<int>tab)
+{
+	for (int i = 0; i < tab.size(); i++)
+	{
+		std::cout << tab[i] << ", ";
+	}
+	bool CzyNalezy(std::vector<int>tab, int element)
+	{
+		for (int i = 0; i < tab.size(); i++)
+		{
+			if ( tab[i] == element)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+int main()
+{
+	std::cout << "I AM GOD and Miciek i Artur is my Adam and Ewa\n";
+
+	std::vector<int>tab = { 1, 3, 5, 7 };
+	std::cout << tab[0] << ", " << tab[1] << ", " << tab[2] << ", " << tab[3] << "\n ";
+	
+	wypisz(tab);
+	
+	bool wynik = CzyNalezy(tab, 4);
+
+
+
+
+}
---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,52 @@
+﻿// ConsoleApplication6.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+void wypisz(int a)
+{
+	std::cout << a << "\n";
+}
+ int suma(int a, int b)
+ {
+	 return a + b;
+}
+ int suma(int a, int b, int c)
+ {
+	 int wynik = suma(a, b);
+	 return suma(wynik, c);
+ }
+
+
+
+
+
+
+int main()
+{
+    std::cout << "micka ma latek\n";
+	
+
+	int a = 5 + 3;
+	int b = suma(a, 5);
+	wypisz(b);
+	b = suma(b, b, b);
+	wypisz(b);
+	//std::cin >> b;
+
+	std::vector <int> tab;
+	tab.push_back(5);
+	wypisz(tab[0]);
+	if (a > 5)
+	{
+		wypisz(a);
+	}
+	else
+	{
+		std::cout << "ale maciek lipa sie okazala!!!\n";
+	}
+}
+
+
